385319974f8587aa34c396d3f28c0d394cb23169
Ali Fnaiech <a.fnaiech@forj.it>
2022-07-15 22:43:42 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
+import { interval, map, mapTo, Subscription, takeUntil, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'timer';
+title = 'timer';
+
+source = timer(0, 1000);
+initialValue = 3
+counter = this.initialValue // value on seconds
+subscription!: Subscription;
+
+startTimer(){
+  this.subscription = this.source.subscribe({
+    next: ()=>{
+      if(this.counter !=0)
+      --this.counter
+      else{
+        this.subscription.unsubscribe();
+      }
+    }
+  });
+}
+
+pauseTimer(){
+  this.subscription.unsubscribe()
+}
+
+resetTimer(){
+    this.subscription.unsubscribe();
+    this.counter = this.initialValue
+  }
+}
+
+@Pipe({
+  name: 'formatTime',
+})
+export class FormatTimePipe implements PipeTransform {
+  transform(value: number): string {
+    const minutes: number = Math.floor(value / 60);
+    return (
+      ('00' + minutes).slice(-2) +
+      ':' +
+      ('00' + Math.floor(value - minutes * 60)).slice(-2)
+    );
+  }
 }
